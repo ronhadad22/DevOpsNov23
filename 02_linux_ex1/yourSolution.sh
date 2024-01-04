89b3916259d311ac602d1308f7cd0bdd423fee86
etcsys <im.se@outlook.com>
2024-01-05 01:46:24 +0200

    New updates

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -1,18 +1,50 @@
-tar -xvf secretGenerator.tar.gz
-#extract secretGGenerator archive
-/bin/bash generateSecret.sh
-#run script generateSecret.sh with bash tool
-mkdir secretDir
-#create new dir naming secretDir
-rm -rf maliciousFiles/
+#!bin/bash
+#
+(
+#Check if File is exist already
+
+if
+#Check if File is exist already if not extract secretGGenerator archive
+  test -e secretGenerator.tar.gz; then
+    echo "file already exist"
+fi
+#Download file
+  wget https://github.com/ronhadad22/DevOpsNov23/blob/main/02_linux_ex1/secretGenerator.tar.gz
+#Extract the Archjve
+  tar -xvf secretGenerator.tar.gz
+#show extracted folder
+  ls -li
+#Enter to extracted folder
+  cd src
+#show content
+  ls -li
+#Change yourSolution file to executable
+   sudo chmod u+x generateSecret.sh
+#create new directory named secret dir
+  mkdir secretDir
 #remove folder named maliciousFiles
-touch secretDir/.secret
+  rm -rf maliciousFiles/
 #create new hiden file named secret inside secretDirectory
-chmod 600 secretDir/.secret
-#set read/write permissions for secret file that was create in previous step
-rm -rf important.link
-#remove important.link
-/bin/bash generateSecret.sh
+  touch secretDir/.secret
+
+
+)
+###
+#/bin/bash generateSecret.sh
 #run script generateSecret.sh with bash tool
-nano secretDir/.secret
+#mkdir secretDir
+#create new dir naming secretDir
+#rm -rf maliciousFiles/
+#remove folder named maliciousFiles
+# secretDir/.secret
+#create new hiden file named secret inside secretDirectory
+#chmod 600 secretDir/.secret
+#set read/write permissions for secret file that was create in previous step
+#rm -rf important.link
+#remove important.link
+#/bin/bash generateSecret.sh
+#run script generateSecret.sh with bash tool
+#nano secretDir/.secret
 #open .secret with nano text editor
+
+
