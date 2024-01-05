8d07ab2b99be87d2bada7656a33df2922f328e0b
etcsys <im.se@outlook.com>
2024-01-05 23:42:44 +0200

    not finished

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -9,36 +9,28 @@ if
       tar -xvf $archive
         cd ./src && sudo chmod u+x $secret
           mkdir $dir
-
-
-
 fi
-
-
-
-
-
 #Download file
 
 #  wget https://github.com/ronhadad22/DevOpsNov23/blob/main/02_linux_ex1/secretGenerator.tar.gz (not worked last time)
 #Extract the Archive
-  tar -xvf secretGenerator.tar.gz
+#  tar -xvf secretGenerator.tar.gz
 #change work directory to src that was extracted from the archive
-  cd src
+#  cd src
 #Change yourSolution file to executable
-   sudo chmod u+x generateSecret.sh
+#   sudo chmod u+x generateSecret.sh
 #create new directory named secret dir
-  mkdir secretDir
+#  mkdir secretDir
 #remove folder named maliciousFiles
-  rm -rf maliciousFiles/
+#  rm -rf maliciousFiles/
 #create new hiden file named secret inside secretDirectory
-  touch secretDir/.secret
+#  touch secretDir/.secret
 #set read/write permissions for secret file that was created.
-  chmod 600 secretDir/.secret
+#  chmod 600 secretDir/.secret
 #remove important.link
-  rm -rf important.link
+#  rm -rf important.link
 #run script generateSecret.sh with bash tool
-  /bin/bash generateSecret.sh
+#  /bin/bash generateSecret.sh
 # secret file content
-  cat secretDir/.secret
+#  cat secretDir/.secret
 
