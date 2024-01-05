93dfecb58b211d2665ad0e5ed66d688980ef4156
etcsys <im.se@outlook.com>
2024-01-06 00:17:32 +0200

    not finished

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -1,43 +1,25 @@
 !bin/bash
-#  wget https://github.com/ronhadad22/DevOpsNov23/blob/main/02_linux_ex1/secretGenerator.tar.gz
+#Seting Variables for archive , dir , secret etc...
 archive = 'secretGenerator.tar.gz'
 secret_gen = 'generateSecret.sh'
 dir = 'secretDir'
 secret = 'secretDir/.secret'
 if
+#Check if File is exist already if not Download secretGGenerator archive
   test -e $archive; then
     echo "file already exist"
-  tar -xvf $archive
+ # else wget https://github.com/ronhadad22/DevOpsNov23/blob/main/02_linux_ex1/secretGenerator.tar.gz (NOT WORKED LAST TIME)
+ #Download file didn't worked last time , link can be updated and line uncomented if needed
+  tar -xvf $archive #Extract the Archive
+#change work directory to src that was extracted from the archive and Change generateSecret.sh file to executable
     cd ./src && sudo chmod u+x $secret_gen
-  mkdir $dir
-    rm -rf maliciousFiles/
+  mkdir $dir #create new directory named secretDr
+    rm -rf maliciousFiles/ #remove folder named maliciousFiles
+    # create new hiden file named secret inside secretDirectory,set read/write permissions
   touch  && chmod 600 $secret
-    rm -rf important.link
-  bin/bash/ $secret_gen
+    rm -rf important.link #remove important.link#remove important.link
+  bin/bash/ $secret_gen #run script generateSecret.sh with bash tool
 fi
-echo $secret
-#Seting Variables for archive , dir , secret etc...
-#Check if File is exist already if not Download secretGGenerator archive
-#Download file
-#  wget https://github.com/ronhadad22/DevOpsNov23/blob/main/02_linux_ex1/secretGenerator.tar.gz (not worked last time)
-#Extract the Archive
-#  tar -xvf secretGenerator.tar.gz
-#change work directory to src that was extracted from the archive
-#  cd src
-#Change generateSecret.sh file to executable
-#   sudo chmod u+x generateSecret.sh
-#create new directory named secret dir
-#  mkdir secretDir
-#remove folder named maliciousFiles
-#  rm -rf maliciousFiles/
-#create new hiden file named secret inside secretDirectory
-#  touch secretDir/.secret
-#set read/write permissions for secret file that was created.
-#  chmod 600 secretDir/.secret
-#remove important.link
-#  rm -rf important.link
-#run script generateSecret.sh with bash tool
-#  /bin/bash generateSecret.sh
-# secret file content
-#  cat secretDir/.secret
+echo $secret #Show secret file content
+
 
