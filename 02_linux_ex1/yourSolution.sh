6f0055293a1622fb5d62bc429a9caf07e5f90eec
etcsys <im.se@outlook.com>
2024-01-06 00:08:54 +0200

    not finished

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -16,14 +16,15 @@ if
   bin/bash/ $secret_gen
 fi
 echo $secret
-#Download file
+#Seting Variables for archive , dir , secret etc...
 #Check if File is exist already if not Download secretGGenerator archive
+#Download file
 #  wget https://github.com/ronhadad22/DevOpsNov23/blob/main/02_linux_ex1/secretGenerator.tar.gz (not worked last time)
 #Extract the Archive
 #  tar -xvf secretGenerator.tar.gz
 #change work directory to src that was extracted from the archive
 #  cd src
-#Change yourSolution file to executable
+#Change generateSecret.sh file to executable
 #   sudo chmod u+x generateSecret.sh
 #create new directory named secret dir
 #  mkdir secretDir
