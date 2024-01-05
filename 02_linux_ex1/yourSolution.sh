1ac928dd26d376dd0312223768030b66375933d2
etcsys <im.se@outlook.com>
2024-01-06 01:53:22 +0200

    not finished (Not worked lst time , working on it)

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -1,52 +1,10 @@
 #!/bin/bash
-#ver1
-#NOT WORK , PLEASE FOLLOW TO VERSION 2
-# Set variables for archive, directory, and secret
-
-export secret_gen=/src/secretDir/generateSecret.sh
-export source=/src
-export secretdir=/src/secretDir
-export secret=/src/secretDir/.secret
-export arch=secretGenerator.tar.gz
-
-# Download secretGenerator archive if it doesn't exist
-wget -N https://github.com/ronhadad22/DevOpsNov23/blob/main/02_linux_ex1/secretGenerator.tar.gz
-
-# Extract the archive
-tar -xvf ${arch}
-
-# Change working directory to src and make generateSecret.sh executable
-cd ${source}
-sudo chmod u+x ${secret_gen}
-
-# Create new directory named secretDir
-mkdir -p ${secretdir}
-
-# Remove folder named maliciousFiles
-rm -rf ./src/maliciousFiles/
-
-# Create new hidden file named .secret inside secretDir and set read/write permissions
-touch ${secret} && chmod 600 ${secret}
-
-# Remove file named important.link
-rm -f ./src/important.link
-
-# Run generateSecret.sh script with bash tool
-./${secret_gen}
-
-# Show contents of .secret file
-cat ${secret}
-
-
-#VERSION 2 Works
-#Seting Variables for archive , dir , secret etc...
-#Check if File is exist already if not Download secretGGenerator archive
 #Download file
 #  wget https://github.com/ronhadad22/DevOpsNov23/blob/main/02_linux_ex1/secretGenerator.tar.gz (not worked last time)
 #Extract the Archive
-tar -xvf secretGenerator.tar.gz
+tar -xvf ./secretGenerator.tar.gz
 #change work directory to src that was extracted from the archive
-cd src
+cd ./src
 #Change generateSecret.sh file to executable
 sudo chmod u+x generateSecret.sh
 #create new directory named secret dir
@@ -63,3 +21,33 @@ rm -rf important.link
 /bin/bash generateSecret.sh
 #show secret file content
 cat secretDir/.secret
+
+#CAUTION--UNDER CONSTRUCTION--CAUTION--UNDER COUNSTRUCTION----CAUTION--UNDER COUNSTRUCTION----CAUTION--UNDER COUNSTRUCTION--
+#NOT WORK , PLEASE FOLLOW TO VERSION 1
+# Set variables for archive, directory, and secret
+export secret_gen=/src/secretDir/generateSecret.sh
+export source=/src
+export secretdir=/src/secretDir
+export secret=/src/secretDir/.secret
+arch=secretGenerator.tar.gz
+# Download secretGenerator archive if it doesn't exist
+wget -N https://github.com/ronhadad22/DevOpsNov23/blob/main/02_linux_ex1/secretGenerator.tar.gz
+# Extract the archive
+tar -xvf ${arch}
+# Change working directory to src and make generateSecret.sh executable
+cd ${source}
+sudo chmod u+x ${secret_gen}
+# Create new directory named secretDir
+mkdir -p ${secretdir}
+# Remove folder named maliciousFiles
+rm -rf ./src/maliciousFiles/
+# Create new hidden file named .secret inside secretDir and set read/write permissions
+touch ${secret} && chmod 600 ${secret}
+# Remove file named important.link
+rm -f ./src/important.link
+# Run generateSecret.sh script with bash tool
+./${secret_gen}
+# Show contents of .secret file
+cat ${secret}
+
+
