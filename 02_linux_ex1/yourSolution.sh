0282a306e48b5e67c3c6c854f3a732eb5a598b68
alexb853 <71152004+alexb853@users.noreply.github.com>
2024-01-07 02:33:02 +0200

    Update yourSolution.sh
    Alex's solution

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -1,10 +1,38 @@
+if test -d secretDir; then
+   echo "Dir is exists"
+else echo "Failed to generate secret. The directory 'secretDir' must exist before."
 mkdir secretDir
-cd maliciousFiles/
-rm amIMaliciousOrNot.whoKnows
-rm someFileIsLinkingToMe.BeAware
-cd ..
-rmdir maliciousFiles/
-touch .secret
-mv .secret secretDir/
-cd secretDir
-chmod 600 .secret
+
+fi
+
+if test -d maliciousFiles; then
+   echo "Failed to generate secret. The directory 'maliciousFiles' contains some malicious files... it must be removed before."
+rm -rf maliciousFailes/
+else echo "maliciousFailes has been removed"
+
+fi
+
+if test -f secretDir/.secret; then
+   echo ".secret exists"
+else echo "Failed to generate secret. The directory 'secretDir' must contain a file '.secret' in which the secret will be stored."
+
+touch secretDir/.secret
+
+fi
+
+OCTAL_PERMISSIONS=$(stat -c "%a" secretDir/.secret)
+if [ "$OCTAL_PERMISSIONS" = "600" ]; then
+   echo "You have read and write permmissions only"
+else echo "Failed to generate secret. The file 'secretDir/.secret' must have read and write permission only."
+chmod 600 secretDir/.secret
+
+fi
+
+if [ -L 'important.link' ] && [ ! -e 'important.link' ]; then
+  echo "Failed to generate secret. Secret can not be generated when broken file link exists. Please do something..."
+  exit 1
+else echo " important.link does not exists"
+
+fi
+
+cat ./CONTENT_TO_HASH | xargs | md5sum > secretDir/.secret && echo "Done! Your secret was stored in secretDir/.secret"
