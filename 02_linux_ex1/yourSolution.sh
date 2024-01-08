7f126794568d3bbe8087b75d30b27b64f212e2b5
etcsys <im.se@outlook.com>
2024-01-08 21:36:53 +0200

    final rev , works as expected

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
+GREEN='\033[0;32m' #seting variable for color
 arch=secretGenerator.tar.gz
 # Download secretGenerator archive if it doesn't exist(NOT WORKED LAST TIME )
-#wget -N https://github.com/ronhadad22/DevOpsNov23/blob/main/02_linux_ex1/secretGenerator.tar.gz
-# Extract the archive and aplly permissions on file generateSecret.sh inside folder src ,  part of the archive
+#wget -N https://github.com/ronhadad22/DevOpsNov23/blob/main/02_linux_ex1/secre>
+# Extract the archive and aplly permissions on file generateSecret.sh inside fo>
 tar -xvf $arch && cd ./src && sudo chmod u+x generateSecret.sh
 #remove malisious Files and important links
 rm -rf maliciousFiles && rm -rf important.link
-#create new dir named secretDir with the new hidden file secret that have only w/r permissions
+#create new dir named secretDir with the new hidden file secret that have only >
 mkdir secretDir && touch secretDir/.secret && chmod 600 secretDir/.secret
 #Run script called generateSecret
 ./generateSecret.sh
 #clear output
 clear
 #Show as output file secret that was created on previous steps
-echo "your secret is : " && cat secretDir/.secret
-
+echo -e "${GREEN}your secret is : " && cat secretDir/.secret
