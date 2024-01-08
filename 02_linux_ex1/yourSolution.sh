494b595c34d7abc00da432a0156121514d3ed76e
etcsys <im.se@outlook.com>
2024-01-08 18:37:32 +0200

    final rev , works as expected

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -2,11 +2,11 @@
 arch=secretGenerator.tar.gz
 # Download secretGenerator archive if it doesn't exist
 #wget -N https://github.com/ronhadad22/DevOpsNov23/blob/main/02_linux_ex1/secretGenerator.tar.gz
-# Extract the archive and aplly permisions on file generateSecret.sh inside folder src tath  part of the archive
+# Extract the archive and aplly permissions on file generateSecret.sh inside folder src ,  part of the archive
 tar -xvf $arch && cd ./src && sudo chmod u+x generateSecret.sh
 #remove malisious Files and important links
 rm -rf maliciousFiles && rm -rf important.link
-#create new dir named secretDir with the new hidden file secret that have only w/r permisions
+#create new dir named secretDir with the new hidden file secret that have only w/r permissions
 mkdir secretDir && touch secretDir/.secret && chmod 600 secretDir/.secret
 #Run script called generateSecret
 ./generateSecret.sh
