dabdf2fae324927b552f138f01beeaca01788b83
etcsys <im.se@outlook.com>
2024-01-10 18:58:41 +0200

    final rev , works as expected

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
+#Setting Variables for folders and files
 GREEN='\033[0;32m' #seting variable for color
 arch=secretGenerator.tar.gz
+source=./src
+secretgen=generateSecret.sh
+files=maliciousFiles
+links=important.link
+dir=secretDir
 # Download secretGenerator archive if it doesn't exist(NOT WORKED LAST TIME )
 #wget -N https://github.com/ronhadad22/DevOpsNov23/blob/main/02_linux_ex1/secre>
-# Extract the archive and aplly permissions on file generateSecret.sh inside fo>
-tar -xvf $arch && cd ./src && sudo chmod u+x generateSecret.sh
+# Extract the archive and aplly permissions on file generateSecret.sh inside folder src
+tar -xvf $arch && cd $source && sudo chmod u+x $secretgen
 #remove malisious Files and important links
-rm -rf maliciousFiles && rm -rf important.link
+rm -rf $files && rm -rf $links
 #create new dir named secretDir with the new hidden file secret that have only >
-mkdir secretDir && touch secretDir/.secret && chmod 600 secretDir/.secret
+mkdir $dir && touch $dir/.secret && chmod 600 $dir/.secret
 #Run script called generateSecret
-./generateSecret.sh
+./$secretgen
 #clear output
 clear
 #Show as output file secret that was created on previous steps
-echo -e "${GREEN}your secret is : " && cat secretDir/.secret
+echo -e "${GREEN}your secret is : " && cat $dir/.secret
