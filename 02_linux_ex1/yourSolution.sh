b741aa2f31139c90aeb1356281529069838ef4e0
DaniXiF <danixif3@gmail.com>
2024-01-28 20:45:22 +0200

    First Commit

---
+++ 02_linux_ex1/yourSolution.sh
@@ -0,0 +1,16 @@
+#Create a secretDir for the Secret to be generated. 
+mkdir secretDir
+
+#Remove the "maliciousFiles" folder before continuing
+rm -rf maliciousFiles
+
+#Create file .secret inside "secretDir"
+touch ./secretDir/.secret 
+
+#Edit file permissions for .secret
+chmod -R 600 ./secretDir/.secret
+
+#Copying the content of .secret to README file
+cat .secret >> /home/dan/linux/ex_linux1/DevOpsNov23/02_linux_ex1/README 
+
+#Wubba
