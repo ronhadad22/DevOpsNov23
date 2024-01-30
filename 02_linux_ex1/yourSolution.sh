776201b75ea273d331ef13fc84e68af056920666
eldiabloj <152429303+eldiabloj@users.noreply.github.com>
2024-01-30 14:43:44 +0200

    Update yourSolution.sh
    osher's solotion

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -1,11 +1,19 @@
 #/bin/bash.sh
 tar xfv secretGenerator.tar.gz
+#go to directory
 cd src
+#creat adirectory secretDir
 mkdir secretDir
+#go to secretDir
 cd secretDir
+#creat file .secrer
 touch .secret
+#authoriz reed  write permission
 chmod 600 .secret
+#go back to src
 cd ..
+#delete mliciousFiles
 rm -rf maliciousFiles
+#delete important.link
 rm -rf important.link
-#/bin/bash generatesecret.sh 
+#generatesecret.sh
