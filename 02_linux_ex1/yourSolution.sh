028c0249cae852ce97f4b7424eb99e975edab050
ronhadad22 <ronhadad22@gmail.com>
2024-02-05 01:21:35 +0200

    Update yourSolution.sh

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -11,10 +11,10 @@ rm -R maliciousFiles
 touch secretDir/.secret
 
 chmod 700 secretDir/.secret
-
+#need to change to 600 -5
 chmod -x secretDir/.secret
 
 mv important.link important.link.bad
-
+#need to unlink the file -5
 ln -s secretDir/.secret important.link
-
+#you need to run the generatesecret.sh -5
