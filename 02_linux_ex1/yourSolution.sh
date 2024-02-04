b56f6c8c95b049caa0470f41692bf8d0670a241c
ronhadad22 <ronhadad22@gmail.com>
2024-02-05 00:55:59 +0200

    check ex

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -12,5 +12,6 @@ chmod -R 600 ./secretDir/.secret
 
 #Copying the content of .secret to README file
 cat .secret >> /home/dan/linux/ex_linux1/DevOpsNov23/02_linux_ex1/README 
-
+#what about the important link?? -5
+#you need to run the generatesecret.sh -5
 #Wubba
