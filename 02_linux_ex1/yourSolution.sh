e03f08f15aaacffc73db4bc44a2a748d53622ac9
rimap <fairy3@gmail.com>
2024-02-05 16:09:04 +0200

    update files

---
+++ 02_linux_ex1/yourSolution.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+secretDir="secretDir"
+secretFile=".secret"
+
+maliciousFiles="maliciousFiles"
+suspiciousLinks="important.link"
+
+# check that a directory "secretDir" exists
+#if doesn't  - create it
+if [ ! -d $secretDir ]; then
+    mkdir $secretDir
+fi
+
+# check that a file .secret exists
+#if it doesn't - create it
+if [ ! -f $secretDir/$secretFile ]; then
+    touch $secretDir/$secretFile
+fi
+
+#change permissions for the secret file:
+#doesn't matter what permission were before - always change it
+#to 600
+chmod 600 $secretDir/$secretFile
+
+#if there are some malicious files exists - delete them
+if [ -d $maliciousFiles ]; then
+        rm -r $maliciousFiles
+fi
+
+#if some suspicious links exist - delete them
+if [ -L $suspiciousLinks ] && [ ! -e $suspiciousLinks ]; then
+   rm -f $suspiciousLinks
+fi
+
+#generate a secret and put it to $secretDir/$secretFile
+#echo print about succesful secret storage.
+cat ./CONTENT_TO_HASH | xargs | md5sum > $secretDir/$secretFile && echo "Done! Your secret was stored in $secretDir/$secretFile"
+
+
