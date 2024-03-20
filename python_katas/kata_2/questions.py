da6353a7dd54bb9578bb63aea09eb02c0e48b564
ronhadad22 <ronhadad22@gmail.com>
2024-03-20 17:36:11 +0200

    Update questions.py

--- python_katas/kata_2/questions.py
+++ python_katas/kata_2/questions.py
@@ -73,7 +73,21 @@ def files_backup(dir_path):
 
 
 
+def replace_in_file(file_path, text, replace_text):
+    """
+    2 Kata
+    This function gets a path of text file, it replaces all occurrences of 'text' by 'replace_text'.
+    The function saves the replaces content on the same path (overwrites the file's content)
 
+    You MUST check that file_path exists in the file system before you try to open it
+
+    :param file_path: relative or absolute path to a text file
+    :param text: text to search
+    :param replace_text: text to replace with
+    :return: None
+    """
+    return None
+    
 
 def json_configs_merge(*json_paths):
     """
